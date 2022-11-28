da35a41ec9860c6723843a7cca84abb39871b7dc
Saurabh gupta <saurabhkgupta.skg@gmail.com>
2022-11-28 22:31:38 +0530

    deploying

---
+++ src/data/allInterests.js
@@ -0,0 +1,7 @@
+const allInterests = [
+    "music", "dance", "fitness" , "gym" , "weightgain", "weightloss" , "longdrives" , "travel" , "destinationWedding",
+    "couple", "romance" , "freestyle" , "sufisongs" , "chailover" , "sad" , "breakup" , "exam" ,
+    "freelancers" , "study" , "groupstudy"
+];
+
+export { allInterests };
---
+++ src/data/chatRooms.js
@@ -0,0 +1,12 @@
+const chatRooms = [
+    { id: 'dogs', title: '🐶 AnimalLovers 🐶' , tags : ["animallover","dog"]},
+    { id: 'food', title: '🍔 Foody 🍔',tags : ["food","chailover"] },
+    { id: 'general', title: '💬 General 💬',tags : ["animallover","dog","study","gym"] },
+    { id: 'news', title: '🗞 News 🗞' ,tags : ["politics","newspaper"]},
+    { id: 'music', title: '🎹 Music 🎹',tags : ["sad","sufisongs","breakup"] },
+    { id: 'sports', title: '🏈 Sports 🏈',tags : ["animallover","dog"] },
+    { id: 'gym', title: 'Fitness',tags : ["weightloss","weightgain","gym"] },
+    { id: 'travel', title: 'Travel',tags : ["longdrives","dog"] },
+];
+
+export { chatRooms };
